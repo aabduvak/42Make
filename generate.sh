bb1b950a80e9e146a9e06eb4d96f3fd8a10aed05
Abdulaziz Abduvakhobov <abdulaziz.yosk@gmail.com>
2022-12-14 23:00:52 +0300

    Added minishell and minilibx templates

--- generate.sh
+++ generate.sh
@@ -102,6 +102,153 @@ elif [ $NAME = "3" ]; then
 	printf "\n✅ ${BOLD}${GREEN}Makefile created successfully...${RESET}\n";
 	display_qr
 
+elif [ $NAME = "4" ] || [ $NAME = "5" ] || [ $NAME = "8" ]; then
+	TEMPLATE="./templates/minilibx.template"
+	
+	if [ $NAME = "4" ]; then
+		PROG="so_long";
+	elif [ $NAME = "5" ]; then
+		PROG="fdf";
+	elif [ $NAME = "8" ]; then
+		PROG="cub3D";
+	fi
+	
+	printf "\nEnter ${BOLD}source${RESET} directory of project: (default folder is ${BOLD}src${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read SOURCES;
+	
+	if [ -z "$SOURCES" ]; then
+		SOURCES="src";
+	fi
+
+	printf "\nEnter ${BOLD}include${RESET} directory of project: (default is ${BOLD}${SOURCES}${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read INCLUDE;
+
+	if [ -z "$INCLUDE" ]; then
+		INCLUDE=$SOURCES;
+	fi
+	
+	printf "\nEnter ${BOLD}libft${RESET} directory of project: (default is ${BOLD}libft${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read LIBFT;
+
+	if [ -z "$LIBFT" ]; then
+		LIBFT="libft";
+	fi
+	
+	printf "\nEnter ${BOLD}minilibx${RESET} directory of project: (default is ${BOLD}minilibx${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read MINILIBX;
+
+	if [ -z "$MINILIBX" ]; then
+		MINILIBX="minilibx";
+	fi
+	
+	cat -s $TEMPLATE | sed 's/name_template/'"$PROG"'/g' \
+							| sed 's/extension_template/'".c"'/g' \
+							| sed 's/cc_template/'"gcc"'/g' \
+							| sed 's/cflags_template/'"-Wall -Wextra -Werror"'/g' \
+							| sed 's/src_template/'"$SOURCES"'/g' \
+							| sed 's/inc_template/'"$INCLUDE"'/g' \
+							| sed 's/lib_template/'"$LIBFT"'/g' \
+							| sed 's/mlx_template/'"$MINILIBX"'/g' \
+							> Makefile;
+	printf "\n✅ ${BOLD}${GREEN}Makefile created successfully...${RESET}\n";
+	display_qr
+elif [ $NAME = "6" ]; then
+	TEMPLATE="./templates/general.template"
+	
+	printf "\nEnter ${BOLD}source${RESET} directory of project: (default folder is ${BOLD}src${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read SOURCES;
+	
+	if [ -z "$SOURCES" ]; then
+		SOURCES="src";
+	fi
+
+	printf "\nEnter ${BOLD}include${RESET} directory of project: (default is ${BOLD}${SOURCES}${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read INCLUDE;
+
+	if [ -z "$INCLUDE" ]; then
+		INCLUDE=$SOURCES;
+	fi
+	
+	cat -s $TEMPLATE | sed 's/name_template/'"philo"'/g' \
+							| sed 's/extension_template/'".c"'/g' \
+							| sed 's/cc_template/'"gcc"'/g' \
+							| sed 's/cflags_template/'"-Wall -Wextra -Werror -pthread"'/g' \
+							| sed 's/src_template/'"$SOURCES"'/g' \
+							| sed 's/inc_template/'"$INCLUDE"'/g' \
+							> Makefile;
+	printf "\n✅ ${BOLD}${GREEN}Makefile created successfully...${RESET}\n";
+	display_qr
+
+elif [ $NAME = "7" ]; then
+	TEMPLATE="./templates/minishell.template"
+	
+	printf "\nEnter ${BOLD}source${RESET} directory of project: (default folder is ${BOLD}src${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read SOURCES;
+	
+	if [ -z "$SOURCES" ]; then
+		SOURCES="src";
+	fi
+
+	printf "\nEnter ${BOLD}include${RESET} directory of project: (default is ${BOLD}${SOURCES}${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read INCLUDE;
+
+	if [ -z "$INCLUDE" ]; then
+		INCLUDE=$SOURCES;
+	fi
+	
+	printf "\nEnter ${BOLD}readline${RESET} directory of project: (default is ${BOLD}readline${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read READLINE;
+
+	if [ -z "$READLINE" ]; then
+		READLINE="readline";
+	fi
+	
+	printf "\nEnter ${BOLD}libft${RESET} directory of project: (default is ${BOLD}libft${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read LIBFT;
+
+	if [ -z "$LIBFT" ]; then
+		LIBFT="libft";
+	fi
+	
+	cat -s $TEMPLATE | sed 's/name_template/'"minishell"'/g' \
+							| sed 's/extension_template/'".c"'/g' \
+							| sed 's/cc_template/'"gcc"'/g' \
+							| sed 's/cflags_template/'"-Wall -Wextra -Werror"'/g' \
+							| sed 's/src_template/'"$SOURCES"'/g' \
+							| sed 's/inc_template/'"$INCLUDE"'/g' \
+							| sed 's/lib_template/'"$LIBFT"'/g' \
+							| sed 's/rdl_template/'"$READLINE"'/g' \
+							> Makefile;
+	printf "\n✅ ${BOLD}${GREEN}Makefile created successfully...${RESET}\n";
+	display_qr
+
+elif [ $NAME = "9" ]; then
+	TEMPLATE="./templates/general.template"
+	
+	printf "\nEnter ${BOLD}source${RESET} directory of project: (default folder is ${BOLD}src${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read SOURCES;
+	
+	if [ -z "$SOURCES" ]; then
+		SOURCES="src";
+	fi
+
+	printf "\nEnter ${BOLD}include${RESET} directory of project: (default is ${BOLD}${SOURCES}${RESET} leave empty if wanted)\n${BLUE}>>${RESET} ";
+	read INCLUDE;
+
+	if [ -z "$INCLUDE" ]; then
+		INCLUDE=$SOURCES;
+	fi
+	
+	cat -s $TEMPLATE | sed 's/name_template/'"main"'/g' \
+							| sed 's/extension_template/'".cpp"'/g' \
+							| sed 's/cc_template/'"c++"'/g' \
+							| sed 's/cflags_template/'"-Wall -Wextra -Werror -std=c++98"'/g' \
+							| sed 's/src_template/'"$SOURCES"'/g' \
+							| sed 's/inc_template/'"$INCLUDE"'/g' \
+							> Makefile;
+	printf "\n✅ ${BOLD}${GREEN}Makefile created successfully...${RESET}\n";
+	display_qr
+
 else
 	printf "❕${YELLOW}Exiting...${RESET}\n";
 fi
